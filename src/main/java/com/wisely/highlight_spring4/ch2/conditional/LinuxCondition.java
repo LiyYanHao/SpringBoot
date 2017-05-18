1f80b7630e9ba302e8e6fd0b6a62ac1f798a773b
lyh <1779077919@qq.com>
2017-05-18 22:13:19 +0800

    条件注解实例

--- src/main/java/com/wisely/highlight_spring4/ch2/conditional/LinuxCondition.java
+++ src/main/java/com/wisely/highlight_spring4/ch2/conditional/LinuxCondition.java
@@ -1,6 +1,5 @@
 package com.wisely.highlight_spring4.ch2.conditional;
 
-
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -8,9 +7,9 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 /**
  * Created by lyh on 17-5-18.
  */
-public class WindowsCondition implements Condition{
+public class LinuxCondition implements Condition{
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        return context.getEnvironment().getProperty("os.name").contains("Windows");
+        return context.getEnvironment().getProperty("os.name").contains("Linux");
     }
 }
---
+++ src/main/java/com/wisely/highlight_spring4/ch2/conditional/ListService.java
@@ -0,0 +1,8 @@
+package com.wisely.highlight_spring4.ch2.conditional;
+
+/**
+ * Created by lyh on 17-5-18.
+ */
+public interface ListService {
+    public String showListCmd();
+}
